6a6daffb69b134132b841a9b27efda252a975906
Andrea Gangemi <andrea.gangemi@gmail.com>
2015-10-14 01:05:56 +0200

    imported sample to read ical

--- cal2homebank.py
+++ cal2homebank.py
@@ -4,3 +4,10 @@ __version__ = '0.0'
 import regex
 
 
+import urllib2  # the lib that handles the url stuff
+
+target_url = 'https://www.google.com/calendar/ical/t4a74d43tjj1vklmv9m435iv3o%40group.calendar.google.com/private-29effcce6007844db3cd1d795a2f3d81/basic.ics'
+
+data = urllib2.urlopen(target_url)  # it's a file like object and works just like a file
+for line in data:  # files are iterable
+    print line
